4711cc7ceab4c8af98d47531c473c4a2c5231f3f
Mister Robo <Mister Robo>
2016-06-27 07:15:20 +0200

    Creation of a project for RazorPlusEngine

--- Parsing.cs
+++ Parsing.cs
@@ -4,28 +4,27 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text.RegularExpressions;
 
-namespace RMA.Classes
+namespace OBAG.RazorPlusEngine
 {
-    public static class RazorPlusEngine
+    public static class Parsing
     {
-
-        private const string _firstHeader = "@using RMA; @{var n1 = 1; var n2 = 1; var n3 = 1; var n4 = 1; var n5 = 1; var n6 = 1; var n7 = 1;"
-                            + " var n8 = 1; var n9 = 1; var n10 = 1; var n11 = 1; var n12 = 1; var n13 = 1; var n14 = 1; var n15 = 1;";
+        private const string _firstHeader = "@using OBAG; "
+                                          + "@{var n1 = 1; var n2 = 1; var n3 = 1; var n4 = 1; var n5 = 1; var n6 = 1; var n7 = 1;"
+                                          + " var n8 = 1; var n9 = 1; var n10 = 1; var n11 = 1; var n12 = 1; var n13 = 1; var n14 = 1; var n15 = 1;";
 
         private const string _itemToHidePattern = "@hide\\[\"(.*?)\"]";
         
         private const string _r_itemToHidePattern = "@repeat\\[\"hide_(.*?)\"]";
 
-        public static string razorPlusParser(SerializableDictionary<string, string> projectItemDic,
-                                             SerializableDictionary<string, Collection<SerializableDictionary<string, string>>> projectRptDic,
-                                             string template,
-                                             string templateFromGenerator
+        public static string razorPlusParser(SerializableDictionary<string, string> projectStaticItemDic,
+                                             SerializableDictionary<string, Collection<SerializableDictionary<string, string>>> projectRepeaterDic,
+                                             string template
                                              )
         {
             string headTemplate;
             string result = null;
 
-            if ((projectItemDic != null && projectItemDic.Count > 0) && (projectRptDic != null && projectRptDic.Count > 0))
+            if ((projectStaticItemDic != null && projectStaticItemDic.Count > 0) && (projectRepeaterDic != null && projectRepeaterDic.Count > 0))
             {
                 List<List<List<SerializableDictionary<string, Collection<SerializableDictionary<string, string>>>>>> razorModel =
                     new List<List<List<SerializableDictionary<string, Collection<SerializableDictionary<string, string>>>>>>();
@@ -35,21 +34,29 @@ namespace RMA.Classes
                 razorModel[0][0].Add(new SerializableDictionary<string, Collection<SerializableDictionary<string, string>>>());
                 razorModel[0][0][0].Add("item", new Collection<SerializableDictionary<string, string>>());
                 razorModel[0][0][0]["item"].Add(new SerializableDictionary<string, string>());
-                razorModel[0][0][0]["item"][0] = projectItemDic;
+                razorModel[0][0][0]["item"][0] = projectStaticItemDic;
 
                 razorModel.Add(new List<List<SerializableDictionary<string, Collection<SerializableDictionary<string, string>>>>>());
                 razorModel[1].Add(new List<SerializableDictionary<string, Collection<SerializableDictionary<string, string>>>>());
-                razorModel[1][0].Add(projectRptDic);
+                razorModel[1][0].Add(projectRepeaterDic);
 
                 headTemplate = _firstHeader
-                             + "var rpt = Model[1][0][0];"
+                             
+                             + "var repeater = Model[1][0][0];"
+                             
                              + "var TEXT = Model[0][0][0][\"item\"][0];"
                              + "var text = Model[0][0][0][\"item\"][0];"
+                             
+                             + "var NUMBER = Model[0][0][0][\"item\"][0];"
+                             + "var number = Model[0][0][0][\"item\"][0];"
+                             
                              + "var DATE = Model[0][0][0][\"item\"][0];"
                              + "var date = Model[0][0][0][\"item\"][0];"
+                             
                              + "var LIST = Model[0][0][0][\"item\"][0];"
                              + "var list = Model[0][0][0][\"item\"][0]; }";
-                            /*+ "PROJECT"
+
+                              /*+ "PROJECT"
                               + "project"
                               + "DIRECTORY"
                               + "directory"
@@ -62,17 +69,18 @@ namespace RMA.Classes
 
                 //////////////////////
 
-                if (template != null) bodyTemplate = Regex.Replace(template, _itemToHidePattern, "");
-
-                else if (templateFromGenerator != null) bodyTemplate = Regex.Replace(templateFromGenerator, _itemToHidePattern, "");
-
-                //////////////////////
-
-                if (bodyTemplate != null) bodyTemplate = Regex.Replace(bodyTemplate, _r_itemToHidePattern, "");
+                if (!String.IsNullOrWhiteSpace(template))
+                    bodyTemplate = Regex.Replace(template, _itemToHidePattern, String.Empty);
 
                 //////////////////////
 
-                string razorTemplate = headTemplate + Environment.NewLine + Environment.NewLine + bodyTemplate;
+                if (!String.IsNullOrWhiteSpace(bodyTemplate))
+                    bodyTemplate = Regex.Replace(bodyTemplate, _r_itemToHidePattern, String.Empty);
+
+                //////////////////////
+
+                string razorTemplate = headTemplate + Environment.NewLine + Environment.NewLine 
+                                     + bodyTemplate;
 
                 ///////////////////
                 // Parsing Start //
@@ -85,56 +93,64 @@ namespace RMA.Classes
                 ///////////////////
             }
 
-            else if (projectItemDic != null && projectItemDic.Count > 0)
+            else if (projectStaticItemDic != null && projectStaticItemDic.Count > 0)
             {
                 headTemplate = _firstHeader
+                             
                              + "var TEXT = Model;"
                              + "var text = Model;"
+                             
+                             + "var NUMBER = Model;"
+                             + "var number = Model;"
+
                              + "var DATE = Model;"
                              + "var date = Model;"
+                             
                              + "var LIST = Model;"
                              + "var list = Model; }";
 
-                string bodyTemplate = "";
+                string bodyTemplate = null;
 
-                if (template != "") bodyTemplate = Regex.Replace(template, _itemToHidePattern, "");
+                if (!String.IsNullOrWhiteSpace(template))
+                    bodyTemplate = Regex.Replace(template, _itemToHidePattern, String.Empty);
 
-                else if (templateFromGenerator != "") bodyTemplate = Regex.Replace(templateFromGenerator, _itemToHidePattern, "");
+                if (!String.IsNullOrWhiteSpace(bodyTemplate))
+                    bodyTemplate = Regex.Replace(bodyTemplate, _r_itemToHidePattern, String.Empty);
 
-                if (bodyTemplate != "") bodyTemplate = Regex.Replace(bodyTemplate, _r_itemToHidePattern, "");
-
-                string razorTemplate = headTemplate + Environment.NewLine + Environment.NewLine + bodyTemplate;
+                string razorTemplate = headTemplate + Environment.NewLine + Environment.NewLine
+                                     + bodyTemplate;
 
                 ///////////////////
                 // Parsing Start //
                 ///////////////////
 
-                result = Razor.Parse(razorTemplate, projectItemDic);
+                result = Razor.Parse(razorTemplate, projectStaticItemDic);
 
                 /////////////////
                 // Parsing End //
                 /////////////////
             }
 
-            else if (projectRptDic != null && projectRptDic.Count > 0)
+            else if (projectRepeaterDic != null && projectRepeaterDic.Count > 0)
             {
-                headTemplate = _firstHeader + "var rpt = Model;}";
+                headTemplate = _firstHeader + "var repeater = Model;}";
 
-                string bodyTemplate = "";
+                string bodyTemplate = null;
 
-                if (template != "") bodyTemplate = Regex.Replace(template, _itemToHidePattern, "");
+                if (!String.IsNullOrWhiteSpace(template))
+                    bodyTemplate = Regex.Replace(template, _itemToHidePattern, String.Empty);
 
-                else if (templateFromGenerator != "") bodyTemplate = Regex.Replace(templateFromGenerator, _itemToHidePattern, "");
+                if (!String.IsNullOrWhiteSpace(bodyTemplate))
+                    bodyTemplate = Regex.Replace(bodyTemplate, _r_itemToHidePattern, String.Empty);
 
-                if (bodyTemplate != "") bodyTemplate = Regex.Replace(bodyTemplate, _r_itemToHidePattern, "");
-
-                string razorTemplate = headTemplate + Environment.NewLine + Environment.NewLine + bodyTemplate;
+                string razorTemplate = headTemplate + Environment.NewLine + Environment.NewLine
+                                     + bodyTemplate;
 
                 ///////////////////
                 // Parsing Start //
                 ///////////////////
 
-                result = Razor.Parse(razorTemplate, projectRptDic);
+                result = Razor.Parse(razorTemplate, projectRepeaterDic);
 
                 /////////////////
                 // Parsing End //
